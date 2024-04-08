3e3af7e0e06604fc7b0e6104b1fde602ddb1b2cc
FarooqRai <farooqahmed25896@gmail.com>
2024-04-08 07:54:39 +0000

    first commit

---
+++ UploadOnServer/Content/BasicLayout/assets/js/custom.js
@@ -0,0 +1,24 @@
+﻿
+$(document).ready(function () {
+
+  
+    /*======================================
+   TEXT EDITOR SCRIPTS
+   ========================================*/
+    $('.textEditorCustom').summernote({
+        height: 350,// set height for editor
+    });
+    /*======================================
+   DATEPICKER SCRIPTS
+   ========================================*/
+    $(".datepicker").datepicker({
+        dateFormat: 'mm/dd/yy',
+        changeYear: true,
+        changeMonth: true,
+       // maxDate: 0,
+       // minDate:-2000 // MINIMUM DATE -2000 days
+    });
+   
+    
+});
+      
---
+++ UploadOnServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PureHRMS.MvcApplication" Language="C#" %>
